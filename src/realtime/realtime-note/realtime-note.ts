71ad5c59a0090abd6cb55bbab649abc86c0fbda8
Philip Molares <philip.molares@udo.edu>
2022-05-24 22:44:43 +0200

    docs: add docs to realtime-note
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/realtime/realtime-note/realtime-note.ts
+++ src/realtime/realtime-note/realtime-note.ts
@@ -13,6 +13,11 @@ import { WebsocketAwareness } from './websocket-awareness';
 import { WebsocketConnection } from './websocket-connection';
 import { WebsocketDoc } from './websocket-doc';
 
+/**
+ * Represents a note currently being edited by a number of clients.
+ *
+ * It holds references to some classes to handle different kinds of messages.
+ */
 export class RealtimeNote {
   protected readonly logger = new Logger(RealtimeNote.name);
   private readonly websocketDoc: WebsocketDoc;
@@ -30,6 +35,12 @@ export class RealtimeNote {
     this.clients.set(client, new WebsocketConnection(client, this));
   }
 
+  /**
+   * Send a sync message to the {@link WebsocketDoc}
+   *
+   * @param client the websocket connection to the client the message came from
+   * @param decoder the decoder of the message
+   */
   public processSyncMessage(client: WebSocket, decoder: Decoder): void {
     const connection = this.clients.get(client);
     if (!connection) {
@@ -38,6 +49,12 @@ export class RealtimeNote {
     this.websocketDoc.processIncomingSyncMessage(connection, decoder);
   }
 
+  /**
+   * Send an awareness message to the {@link WebsocketAwareness}
+   *
+   * @param client the websocket connection to the client the message came from
+   * @param decoder the decoder of the message
+   */
   public processAwarenessMessage(client: WebSocket, decoder: Decoder): void {
     const connection = this.clients.get(client);
     if (!connection) {
@@ -54,7 +71,6 @@ export class RealtimeNote {
    *
    * @param {WebSocket} client The websocket client that disconnects.
    * @param {() => void} onDestroy Will be executed if the realtime note is empty and should be deleted.
-   * @return {@code true} when the client was the last one in this realtime note, {@code false} otherwise.
    */
   public removeClient(client: WebSocket, onDestroy: () => void): void {
     this.clients.delete(client);
@@ -67,6 +83,11 @@ export class RealtimeNote {
     }
   }
 
+  /**
+   * Checks if there's still clients connected to this note.
+   *
+   * @return {@code true} if there a still clinets connected, otherwise {@code false}
+   */
   public hasConnections(): boolean {
     return this.clients.size !== 0;
   }
@@ -80,18 +101,38 @@ export class RealtimeNote {
     return this.noteId;
   }
 
+  /**
+   * Returns all {@link WebsocketConnection WebsocketConnections} currently hold by this note.
+   *
+   * @return an array of {@link WebsocketConnection WebsocketConnections}
+   */
   public getConnections(): WebsocketConnection[] {
     return [...this.clients.values()];
   }
 
+  /**
+   * Get the {@link Doc YDoc} of the note.
+   *
+   * @return the {@link Doc YDoc} of the note
+   */
   public getYDoc(): Doc {
     return this.websocketDoc;
   }
 
+  /**
+   * Get the {@link Awareness YAwareness} of the note.
+   *
+   * @return the {@link Awareness YAwareness} of the note
+   */
   public getAwareness(): Awareness {
     return this.websocketAwareness;
   }
 
+  /**
+   * Get the current content of the note.
+   *
+   * @return current content of the note
+   */
   public getNoteContent(): string {
     return this.websocketDoc.getCurrentContent();
   }
